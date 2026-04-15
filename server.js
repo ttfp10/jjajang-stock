fafc3e1d22830b2a5f35a93a4b3ce1531d9933d2
ttfp10 <ysts4609@gmail.com>
2026-04-15 20:15:55 +0900

    server.js

--- server.js
+++ server.js
@@ -214,4 +214,6 @@ setInterval(()=>{
   });
 },2000);
 
-server.listen(3000, ()=>console.log("http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, ()=>console.log("server running"));
+
